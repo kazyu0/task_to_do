40fb70c2843a1420f65b1225fd217e39276aab8a
kazyu0 <kazumakimijima.sb@gmail.com>
2025-02-27 18:36:50 +0900

    追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: 'タスクが追加されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Strong Parameters: 許可するパラメータを指定
+  def task_params
+    params.require(:task).permit(:title, :details, :deadline, :status)
+  end
 end
